47994f785886b5789ef1bcc701e6d19b6e15cca8
nutcakez <knorbert91@gmail.com>
2025-10-29 22:29:11 +0100

    options?

--- aoedude.go
+++ aoedude.go
@@ -3,12 +3,19 @@ package main
 // doing aoe damage around the target
 type aoeDude struct {
 	dmg, hp, attackRange, area int
+	// putting something into the struct to figure out a way to get units around the target
+	// 1 unitsInRange(int, int) []Unit func
+	// 2 just embed the whole unitManager here yolo
 }
 
 func (d *aoeDude) TakeDmg(dmgTaken int) {
 	d.hp -= dmgTaken
 }
 
+// other
+// 3 pass ALLLLLLLL the targets, unit itself will figure out what to attack/who takes dmg
+// 4 return with a value and unit manager will find the aoe targets, dont like it cuz Unit related logic
+// bleeds into unitmanager
 func (d *aoeDude) Attack(target Unit) {
 	target.TakeDmg(d.dmg)
 	// i want to do dmg to the rest T_T
